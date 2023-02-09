0030e7f993d11f3e5fe8b81cbfebd9f6f0c3f08f
方啸云 <fangxiaoyun@yuctime.com>
2023-02-09 18:06:17 +0800

    项目GIT代码检测配置

--- app/src/main/java/com/ycfangxiaoyun/gitdemo/SplashMainActivity.kt
+++ app/src/main/java/com/ycfangxiaoyun/gitdemo/SplashMainActivity.kt
@@ -3,9 +3,9 @@ package com.ycfangxiaoyun.gitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_main)
     }
 }
